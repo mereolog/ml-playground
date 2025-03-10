9f69af52a0f07582535064017b47275acb296ee1
Paul02200 <bpawel754@wp.pl>
2025-03-10 22:25:21 +0100

    Update app.py

--- MVP/Team#7/app.py
+++ MVP/Team#7/app.py
@@ -251,7 +251,7 @@ def load_data(uploaded_file):
     y = data['Salary'].values
     return data, X, y
 
-def train_model(g.X, y, regularization_type, alpha):
+def train_model(X, y, regularization_type, alpha):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     if regularization_type == "Lasso":
         model = Lasso(alpha=alpha)
