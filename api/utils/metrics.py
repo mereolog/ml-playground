50d9f40663997c71f205b57c6a3f18b59a6aa7ce
Artur Podsiadły <artpods56@gmail.com>
2025-03-26 02:26:00 +0100

    feat: add loss and metrics

---
+++ api/utils/metrics.py
@@ -0,0 +1,44 @@
+"""
+This module provides functions for calculating common evaluation metrics
+for machine learning models.
+"""
+
+from typing import Dict, Literal, Optional, Tuple, Union
+
+import numpy as np
+from utils.losses import MeanAbsoluteError, MeanSquaredError
+
+# Optional: Some metrics are already implemented in the loss classes
+# example: MSE/MAE are both losses and metrics
+
+
+# -- regression metrics --
+
+
+def mean_squared_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    Calculate the Mean Squared Error (MSE) by reusing the Mean Squared Error class.
+
+    MSE = mean((y_pred - y_true)^2)
+
+    Args:
+        y_true: Ground truth target values, shape (n_samples,).
+        y_pred: Predicted values, shape (n_samples,).
+
+    Returns:
+        Mean Squared Error value.
+    """
+
+    mse_loss_calculator = MeanSquaredError()
+
+    return mse_loss_calculator(y_true, y_pred)
+
+
+def mean_absolute_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    class doc string to be filled
+    """
+
+    mae_loss_calculator = MeanAbsoluteError()
+
+    return mae_loss_calculator(y_true, y_pred)
