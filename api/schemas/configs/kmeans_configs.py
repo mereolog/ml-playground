785bb1f77066f8ddb32ff7fd86b287c07389cb07
Paul02200 <bpawel754@wp.pl>
2025-04-02 13:23:30 +0200

    Update kmeans_configs.py

--- api/schemas/configs/kmeans_configs.py
+++ api/schemas/configs/kmeans_configs.py
@@ -1,1 +1,26 @@
+from dataclasses import dataclass
+from typing import Optional
 
+@dataclass
+class KMeansParams(UnsupervisedAlgorithmsParams):
+    """This model parameters config defines types and possible default arguments used by the KMeans clustering model
+
+    Attributes:
+        n_clusters: Number of clusters to form (default: 8)
+        max_iter: Maximum number of iterations of the k-means algorithm (default: 300)
+        tol: Tolerance to declare convergence (default: 1e-4)
+        init: Method for initialization ('k-means++', 'random') (default: 'random')
+        n_init: Number of times the k-means algorithm will be run with different centroid seeds (default: 10)
+        metric: Distance metric to use ('euclidean', 'manhattan') (default: 'euclidean')
+    """
+
+    n_clusters: int = 8
+    max_iter: int = 300
+    tol: float = 1e-4
+    init: str = "random"
+    n_init: int = 10
+    metric: str = "euclidean"
+
+
+def __post_init__(self):
+        super().__post_init__()
