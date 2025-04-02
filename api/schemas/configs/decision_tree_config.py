27f88fa2b71157fe208cb45cdda026e1cfcfad83
kubalewczynski <kubs0n13@gmail.com>
2025-04-02 21:51:31 +0200

    add DecisionTree params

---
+++ api/schemas/configs/decision_tree_config.py
@@ -0,0 +1,24 @@
+from algorithms_configs import SupervisedAlgorithmsParams
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class DecisionTreeParams(SupervisedAlgorithmsParams):
+
+    """
+    Attributes:
+        max_tree_depth: Maximum depth of a tree (default: None)
+        min_leaf_samples: Minimal number of samples needed to create new leaf (default: 1)
+        min_split_samples: Minimal number of samples needed to make a split (default: 2)
+
+
+    """
+
+    max_tree_depth: Optional[int] = None
+    min_leaf_samples: int = 1
+    min_split_samples: int = 2
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.max_tree_depth <= 0:
+            raise ValueError("Must be positive integer.")
