b3fa07d46e55a9ecfdd3933e92c1864956490b59
Paul02200 <bpawel754@wp.pl>
2025-04-02 20:03:51 +0200

    Update kmeans_configs.py

--- api/schemas/configs/kmeans_configs.py
+++ api/schemas/configs/kmeans_configs.py
@@ -10,17 +10,17 @@ class KMeansParams(UnsupervisedAlgorithmsParams):
         n_clusters: Number of clusters to form (default: 8)
         max_iter: Maximum number of iterations of the k-means algorithm (default: 300)
         tol: Tolerance to declare convergence (default: 1e-4)
-        init: Method for initialization of centroids ('k-means++', 'random') (default: 'random')
-        n_init: Number of times the k-means algorithm will be run with different centroid seeds (default: 10)
+        init_method: Method for initialization of centroids ('k-means++', 'random') (default: 'random')
+        initialization_runs: Number of times the k-means algorithm will be run with different centroid seeds (default: 10)
         metric: Distance metric to use ('euclidean', 'manhattan') (default: 'euclidean')
     """
 
     n_clusters: int = 8
     max_iter: int = 300
     tol: float = 1e-4
-    init: str = "random"
-    n_init: int = 10
-    metric: str = "euclidean"
+    init_method: Literal["k-means++", "random"] = "random"
+    initialization_runs: int = 10
+    metric: Literal["euclidean", "manhattan"] = "euclidean"
 
 
 def __post_init__(self):
@@ -30,9 +30,9 @@ def __post_init__(self):
         raise ValueError("max_iter must be a positive integer")
     if self.tol <= 0:
         raise ValueError("tol must be a positive float")
-    if self.init not in {"k-means++", "random"}:
+    if self.init_method not in {"k-means++", "random"}:
         raise ValueError("init must be 'k-means++' or 'random'")
-    if self.n_init <= 0:
+    if self.initialization_runs <= 0:
         raise ValueError("n_init must be a positive integer")
     if self.metric not in {"euclidean", "manhattan"}:
         raise ValueError("metric must be 'euclidean' or 'manhattan'")
