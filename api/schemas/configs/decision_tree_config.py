dab6e7f7524d0d28a06db2ccd1e3f92655a295d6
kubalewczynski <kubs0n13@gmail.com>
2025-04-03 12:14:40 +0200

    refactor: change relative imports to the slightly less absolute again

--- api/schemas/configs/decision_tree_config.py
+++ api/schemas/configs/decision_tree_config.py
@@ -1,4 +1,4 @@
-from algorithms_configs import SupervisedAlgorithmsParams
+from schemas.configs.algorithms_configs import SupervisedAlgorithmsParams
 from dataclasses import dataclass
 from typing import Optional
 
