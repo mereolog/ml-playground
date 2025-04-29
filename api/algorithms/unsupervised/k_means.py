f129c28f2c04f2960025ed23b6e039b66367b00a
Pawel <bpawel754@wp.pl>
2025-04-29 18:45:55 +0200

    Added k_means file

---
+++ api/algorithms/unsupervised/k_means.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class KMeans:
+    def __init__(self, k=3, max_iters=100, tol=1e-4,
+                 init_method='random', metric='euclidean'):
+        self.k = k
+        self.max_iters = max_iters
+        self.tol = tol
+        self.init_method = init_method
+        self.metric = metric
+
+        self.centroids = None
+        self.labels = None
+        self.inertia_ = None
+
+    def _initialize_centroids(self, X):
+        if self.init_method == 'random':
+            indices = np.random.choice(len(X), self.k, replace=False)
+            return X[indices]
+        elif self.init_method == 'k-means++':
+            centroids = [X[np.random.randint(len(X))]]
+            for _ in range(1, self.k):
+                distances = np.min(self._compute_distances(X, np.array(centroids)), axis=1)
+                probs = distances ** 2
+                probs /= np.sum(probs)
+                next_index = np.random.choice(len(X), p=probs)
+                centroids.append(X[next_index])
+            return np.array(centroids)
+        else:
+            raise ValueError("Invalid init_method. Use 'random' or 'k-means++'.")
+
+    def _compute_distances(self, X, centroids):
+        if self.metric == 'euclidean':
+            return np.linalg.norm(X[:, np.newaxis] - centroids, axis=2)
+        elif self.metric == 'manhattan':
+            return np.sum(np.abs(X[:, np.newaxis] - centroids), axis=2)
+        else:
+            raise ValueError("Invalid metric. Use 'euclidean' or 'manhattan'.")
+
+    def plot_step(self, X, centroids, labels, iteration):
+        plt.figure(figsize=(6, 5))
+        for i in range(self.k):
+            plt.scatter(X[labels == i, 0], X[labels == i, 1], label=f'Cluster {i}')
+        plt.scatter(centroids[:, 0], centroids[:, 1], c='black', marker='X', s=200, label='Centroids')
+        plt.title(f'Iteration {iteration}')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    def fit(self, X, visualize=True):
+        self.centroids = self._initialize_centroids(X)
+
+        for iteration in range(self.max_iters):
+            distances = self._compute_distances(X, self.centroids)
+            labels = np.argmin(distances, axis=1)
+
+            if visualize:
+                self.plot_step(X, self.centroids, labels, iteration)
+
+            new_centroids = np.array([
+                X[labels == i].mean(axis=0) if np.any(labels == i) else self.centroids[i]
+                for i in range(self.k)
+            ])
+
+            shift = np.linalg.norm(self.centroids - new_centroids)
+            self.centroids = new_centroids
+            self.labels = labels
+
+            if shift < self.tol:
+                break
+
+        self.inertia_ = np.sum(np.min(self._compute_distances(X, self.centroids), axis=1) ** 2)
+
+    def predict(self, X):
+        distances = self._compute_distances(X, self.centroids)
+        return np.argmin(distances, axis=1)
