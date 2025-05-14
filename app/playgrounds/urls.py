129cfbb885b9f08b250ccdeb05abfc394331481c
Artur Podsiadły <artpods56@gmail.com>
2025-05-14 09:20:35 +0200

    feat: add home view and algorithm form handling, update URLs and templates

--- app/playgrounds/urls.py
+++ app/playgrounds/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
-from .views import linear_regression
+from .views import linear_regression, algorithm_form
 
-urlpatterns = [path("linear_regression/", linear_regression, name="linear-regression")]
+urlpatterns = [path("linear_regression/", linear_regression, name="linear-regression"),
+                path("algorithm-form/<str:algorithm_name>/", algorithm_form, name="algorithm_form"),]
+    
--- app/playgrounds/views.py
+++ app/playgrounds/views.py
@@ -1,7 +1,22 @@
 import os
-
+import requests
+import json
 from django.shortcuts import render
+from django.http import HttpResponse
 
+FASTAPI_URL = "http://fastapi:8000" 
+
+
+def home_view(request):
+    available_algorithms = requests.get(f"{FASTAPI_URL}/algorithms").json()
+    print(available_algorithms)
+
+    context = {
+        "algorithms": available_algorithms,
+    }
+    print(type(available_algorithms))
+
+    return render(request, "pages/playground.html", context=context)
 
 def linear_regression(request):
     dataset_dir = os.getenv("DATASET_DIR", "/app/datasets")
@@ -10,3 +25,12 @@ def linear_regression(request):
     context = {"datasets": available_datasets}
 
     return render(request, "algorithms/linear_regression.html", context=context)
+
+
+def algorithm_form(request, algorithm_name):
+    resp = requests.get(f"{FASTAPI_URL}/algorithms/{algorithm_name}/config_schema")
+    if resp.status_code != 200:
+        return HttpResponse("Could not fetch schema", status=500)
+    schema = resp.json()
+    print(schema)
+    return render(request, "partials/algorithm_inputs.html", {"schema": schema})
