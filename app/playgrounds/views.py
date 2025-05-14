725cd5b34b5798d578fabdf7fec11ccceaa23ffd
Artur Podsiadły <artpods56@gmail.com>
2025-05-14 13:04:33 +0200

    fix: small tweaks to make the dataset loading work

--- app/playgrounds/views.py
+++ app/playgrounds/views.py
@@ -1,28 +1,26 @@
-import os
-import requests
 import json
-from django.shortcuts import render
-from django.http import HttpResponse
+import os
 
-FASTAPI_URL = "http://fastapi:8000" 
+import requests
+from django.http import HttpResponse
+from django.shortcuts import render
+
+FASTAPI_URL = "http://fastapi:8000"
 
 
 def home_view(request):
     available_algorithms = requests.get(f"{FASTAPI_URL}/algorithms").json()
     print(available_algorithms)
 
-    context = {
-        "algorithms": available_algorithms,
-    }
-    print(type(available_algorithms))
-
-    return render(request, "pages/playground.html", context=context)
-
-def linear_regression(request):
     dataset_dir = os.getenv("DATASET_DIR", "/app/datasets")
 
     available_datasets = [f for f in os.listdir(dataset_dir) if f.endswith(".csv")]
-    context = {"datasets": available_datasets}
+    context = {"algorithms": available_algorithms, "datasets": available_datasets}
+
+    return render(request, "pages/playground.html", context=context)
+
+
+def linear_regression(request):
 
     return render(request, "algorithms/linear_regression.html", context=context)
 
@@ -34,3 +32,4 @@ def algorithm_form(request, algorithm_name):
     schema = resp.json()
     print(schema)
     return render(request, "partials/algorithm_inputs.html", {"schema": schema})
+
