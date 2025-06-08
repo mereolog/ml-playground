8a5cbd65c058a1ab8e3bea584624bcf71cb218c0
Pilimaan <115808390+Pilimaan@users.noreply.github.com>
2025-06-08 21:33:38 +0200

    Update hierarchical_clustering.py

--- api/algorithms/unsupervised/hierarchical_clustering.py
+++ api/algorithms/unsupervised/hierarchical_clustering.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 import numpy as np
 
 from algorithms.base.algorithm import Algorithm
-from schemas.configs.algorithm_configs import SupervisedAlgorithmsParams, 
+from schemas.configs.algorithm_configs import SupervisedAlgorithmsParams 
 from schemas.configs.hierarchical_clastering_config import HierarchicalClusteringParams
 
 class HierarchicalClustering(UnsupervisedAlgorithm):
